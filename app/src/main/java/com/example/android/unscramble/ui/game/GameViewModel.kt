461c6f822c4c48afeead2b023bf005e7f6ec5f87
GithubPranshul <pranshulsingh9060@gmail.com>
2021-05-27 08:12:38 +0530

    This is unscramble game app.

---
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.android.unscramble.ui.game
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel()
+{
+    init {
+        Log.d("GameFragment","GameViewModel created!")
+    }
+
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
+     get() = _score
+
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+    get() = _currentWordCount
+
+
+     private  val _currentScrambleWord= MutableLiveData<String>()
+       val currentScrambledWord: LiveData<Spannable> = Transformations.map(_currentScrambleWord){
+           if(it==null){
+               SpannableString("")
+           } else {
+               val scrambleWords = it.toString()
+               val spannable: Spannable = SpannableString(scrambleWords)
+               spannable.setSpan(
+                   TtsSpan.VerbatimBuilder(scrambleWords).build(),
+                   0,
+                   scrambleWords.length,
+                   Spannable.SPAN_INCLUSIVE_INCLUSIVE
+               )
+               spannable
+           }
+       }
+
+
+    private var wordList: MutableList<String> = mutableListOf()
+            private lateinit var currentWord:String
+          init {
+              getNextWord()
+          }
+            private fun getNextWord(){
+                currentWord= allWordsList.random()
+                val tempWord=currentWord.toCharArray()
+                tempWord.shuffle()
+
+                while (String(tempWord).equals(currentWord,false)){
+                    tempWord.shuffle()
+                }
+                if (wordList.contains(currentWord))
+                {
+                    getNextWord()
+                }
+                else
+                {
+                    _currentScrambleWord.value = String(tempWord)
+                     _currentWordCount.value = (_currentWordCount.value)?.inc()
+                      wordList.add(currentWord)
+                }
+
+            }
+    fun reinitializeData(){
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordList.clear()
+        getNextWord()
+
+    }
+
+    private fun increaseScore(){
+        _score.value = _score.value?.plus((SCORE_INCREASE))
+    }
+    fun isUserWordCorrect(playerWord:String): Boolean{
+        if (playerWord.equals(currentWord,true)){
+            increaseScore()
+            return true
+        }
+        return false
+    }
+    fun nextWord(): Boolean {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+}
